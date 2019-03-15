f11d4701f24ec958adbb52430622d4fed6675fd2
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-03-15 15:54:08 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,16 @@
-var colorPalette = document.getElementById("color");
-var colorChoice = document.getElementById("colorChange").setAttribute("value", colorChoiceVal);
-var colorChoiceVal = colorChoice.value;
-var currentColor = "./styles/color" + colorChoiceVal + ".css";
-console.log(colorChoiceVal);
-if ((colorChoiceVal = "undefined")) {
-  colorChoiceVal = 1;
-} else {
-  colorChoiceVal = colorChoice.value;
-}
-
-function changeColor() {
-  colorPalette.setAttribute("href", currentColor);
+function changeColor(colorPalette) {
+  function getColorVal(colorChoiceVal) {
+    var colorChoice = document.getElementById("colorChange").setAttribute("value", colorChoiceVal);
+    var colorChoiceVal = colorChoice.value;
+  }
+  colorPalette.document.getElementById("color").setAttribute("href", currentColor);
+  var currentColor = "./styles/color" + colorChoiceVal + ".css";
+  console.log(colorChoiceVal);
+  if ((colorChoiceVal = "undefined")) {
+    colorChoiceVal = 1;
+  } else {
+    colorChoiceVal = colorChoice.value;
+  }
 }
 colorChoice.preventDefault;
 colorChoice.addEventListener("submit", changeColor());
